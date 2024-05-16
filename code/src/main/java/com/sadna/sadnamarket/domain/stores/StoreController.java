332115fed1130b88cdc17bd6ace19c2efa6b2baa
EyalShap <eyald@post.bgu.ac.il>
2024-05-16 16:47:33 +0300

    added example files

---
+++ code/src/main/java/com/sadna/sadnamarket/domain/stores/StoreController.java
@@ -0,0 +1,8 @@
+package com.sadna.sadnamarket.domain.stores;
+
+//this is the facade of the Stores package
+//it merely exists so the folder appears on git
+//you may delete these comments when beggining work
+//have fun :)
+public class StoreController {
+}
