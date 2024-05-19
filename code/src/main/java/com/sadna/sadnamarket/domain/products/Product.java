4ca3459585e89b22d51d0c9f8bd37eedb467e5e5
Matan <matan203040@gmail.com>
2024-05-19 21:14:16 +0300

    add product

--- code/src/main/java/com/sadna/sadnamarket/domain/products/Product.java
+++ code/src/main/java/com/sadna/sadnamarket/domain/products/Product.java
@@ -1,8 +1,36 @@
 package com.sadna.sadnamarket.domain.products;
 
 public class Product {
+    private String productName;
+    private int productQuantity ;
+    private int productPrice;
+    public Product(String productName,int productQuantity,int productPrice){
+        this.productName=productName;
+        this.productQuantity=productQuantity;
+        this.productPrice=productPrice;
+    }
 
-    public Product(){
+    public String getProductName() {
+        return productName;
+    }
 
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getProductAmount() {
+        return productQuantity;
+    }
+
+    public void setProductAmount(int productAmount) {
+        this.productQuantity = productAmount;
+    }
+
+    public int getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(int productPrice) {
+        this.productPrice = productPrice;
     }
 }
--- code/src/main/java/com/sadna/sadnamarket/domain/products/ProductController.java
+++ code/src/main/java/com/sadna/sadnamarket/domain/products/ProductController.java
@@ -1,11 +1,22 @@
 package com.sadna.sadnamarket.domain.products;
 
+import com.sadna.sadnamarket.domain.stores.Store;
+
+import java.util.HashMap;
+import java.util.List;
+
 //this is the facade of the Products package
 //it merely exists so the folder appears on git
 //you may delete these comments when beggining work
 //have fun :)
 public class ProductController {
     private static ProductController instance;
+    //need to check
+    private HashMap<Integer, List<Product>> products;
+
+    private ProductController(){
+        products = new HashMap<>();
+    }
 
     public static ProductController getInstance() {
         if (instance == null) {
@@ -13,4 +24,11 @@ public class ProductController {
         }
         return instance;
     }
+
+    public void addProductToStore(int storeId , String productName,int productQuantity,int price ){
+        Product createdProduct=new Product(productName,productQuantity,price);
+       // products.put(storeId,);
+    }
+
+
 }
