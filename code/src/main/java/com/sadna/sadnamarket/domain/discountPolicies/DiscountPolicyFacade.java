3e9712813270e213700eb5bce29ebe7d78985a90
NirAharoni2 <nirahar@post.bgu.ac.il>
2024-05-25 18:05:54 +0300

    discount update

--- code/src/main/java/com/sadna/sadnamarket/domain/discountPolicies/DiscountPolicyFacade.java
+++ code/src/main/java/com/sadna/sadnamarket/domain/discountPolicies/DiscountPolicyFacade.java
@@ -1,22 +1,24 @@
 package com.sadna.sadnamarket.domain.discountPolicies;
 
+import com.sadna.sadnamarket.api.Response;
 import com.sadna.sadnamarket.domain.products.ProductDTO;
 import com.sadna.sadnamarket.domain.products.ProductFacade;
 import com.sadna.sadnamarket.domain.users.CartItemDTO;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class DiscountPolicyFacade {
 
-    private Map<Integer, DiscountPolicy> mapper;
+    private Map<Integer, DiscountManager> mapper;
     private ProductFacade productFacade;
     private static DiscountPolicyFacade instance;
 
     public DiscountPolicyFacade() {
-        mapper = new HashMap<Integer, DiscountPolicy>();
+        mapper = new HashMap<Integer, DiscountManager>();
         productFacade = ProductFacade.getInstance();
     }
 
@@ -27,25 +29,23 @@ public class DiscountPolicyFacade {
         return instance;
     }
 
-    public int addDiscountPolicy() {
-        // proxy function
-        // assuming it returns the id of the new policy
-        return 0;
+    public boolean addDiscountPolicy(int storeId, String args) {
+        DiscountPolicy dp;
+        if (mapper.get(storeId) == null) {
+            mapper.put(storeId, new DiscountManager());
+        }
+        try {
+            dp = new DiscountPolicy(args);
+        } catch (Exception e) {
+            return false;
+        }
+        DiscountManager discountManager = mapper.get(storeId);
+        discountManager.addDiscountPolicy(dp);
+        return true;
     }
 
-    public List<ProductDataPrice> calculatePrice(List<Integer> discountPolicyIds, List<CartItemDTO> cart) {
-        List<ProductDataPrice> recipe = new ArrayList<ProductDataPrice>();
-        for (CartItemDTO cartItem : cart) {
-            for (Integer dpid : discountPolicyIds) {
-                DiscountPolicy discountPolicy = mapper.get(dpid);
-                ProductDTO productDTO = productFacade.getProductDTO(cartItem.getProductId());
-                recipe.add(new ProductDataPrice(cartItem.getProductId(),
-                        cartItem.getAmount(),
-                        productDTO.getProductPrice(),
-                        discountPolicy.giveDiscount(productDTO, cartItem)));
-
-            }
-        }
-        return recipe;
+    public List<ProductDataPrice> calculatePrice(int storeId, List<CartItemDTO> cart) {
+        DiscountManager discountManager = mapper.get(storeId);
+        return discountManager.giveDiscount(cart, productFacade);
     }
 }
