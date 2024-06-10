ed2932ccf363253b8c0ea35760d4b97373d618b4
Matan <matan203040@gmail.com>
2024-06-10 22:46:48 +0300

    add changes to user api

---
+++ code/src/main/java/com/sadna/sadnamarket/api/ProductRequest.java
@@ -0,0 +1,34 @@
+package com.sadna.sadnamarket.api;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class ProductRequest {
+
+    int storeId;
+    int productId;
+    int amount;
+
+    public int getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(int storeId) {
+        this.storeId = storeId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
