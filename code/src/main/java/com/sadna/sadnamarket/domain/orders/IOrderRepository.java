a8ecd87f246fd7cf17b884646f06e0a5d174ef25
Matan <matan203040@gmail.com>
2024-06-13 16:11:19 +0300

    Changes to orders

--- code/src/main/java/com/sadna/sadnamarket/domain/orders/IOrderRepository.java
+++ code/src/main/java/com/sadna/sadnamarket/domain/orders/IOrderRepository.java
@@ -1,5 +1,7 @@
 package com.sadna.sadnamarket.domain.orders;
 
+import com.sadna.sadnamarket.domain.discountPolicies.ProductDataPrice;
+
 import java.util.List;
 import java.util.Map;
 
@@ -7,7 +9,7 @@ public interface IOrderRepository {
     int createOrder(Map<Integer, OrderDTO> storeOrdersDTO);
     List<OrderDTO> getOrders(int storeId);
 
-    Map<Integer,Map<Integer,OrderDTO>> getOrdersByMember(String nameMember);
+    Map<Integer,List<ProductDataPrice>> getProductDataPriceByMember(String nameMember);
     Map<Integer,OrderDTO> getOrderByOrderId(int orderId);
     List<OrderDTO> getAllOrders();
 }
