12cab07bb96b445cadd2d44a56bf2c443de72b56
Matan <matan203040@gmail.com>
2024-07-02 19:44:03 +0300

    Change from DTO in data

--- code/src/main/java/com/sadna/sadnamarket/domain/orders/Order.java
+++ code/src/main/java/com/sadna/sadnamarket/domain/orders/Order.java
@@ -1,14 +1,42 @@
 package com.sadna.sadnamarket.domain.orders;
 
 
+import javax.persistence.*;
 import java.util.Map;
 
+@Entity
+@Table(name = "Orders")
 public class Order {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column(name = "member_name")
     private String memberName;
+
+    @Column(name = "store_id")
+    private Integer storeId;
+    @Column(name = "store_name_when_ordered")
     private String storeNameWhenOrdered;
+    @ElementCollection
+    @CollectionTable(name = "Order_Product_Amounts", joinColumns = @JoinColumn(name = "order_id"))
+    @MapKeyColumn(name = "product_id")
+    @Column(name = "amount")
     private Map<Integer, Integer> productAmounts;
+    @ElementCollection
+    @CollectionTable(name = "Order_Products_Jsons", joinColumns = @JoinColumn(name = "order_id"))
+    @MapKeyColumn(name = "product_id")
+    @Column(name = "json")
     private Map<Integer, String> orderProductsJsons;
+
+
+    @ManyToOne
+    @JoinColumn(name = "order_wrapper_id")
+    private OrderWrapper orderWrapper;
+
+    public Order(){
+
+    }
 
     public Order(String memberName,String storeNameWhenOrdered,Map<Integer, Integer> productAmounts,Map<Integer, String> orderProductsJsons){
         this.memberName=memberName;
@@ -33,8 +61,10 @@ public class Order {
         return orderProductsJsons;
     }
 
-    public static void main(String[] args) {
-        System.out.println("sdfsdfds");
-    }
+    
+
+   // public static void main(String[] args) {
+//        System.out.println("sdfsdfds");
+//    }
 
 }
